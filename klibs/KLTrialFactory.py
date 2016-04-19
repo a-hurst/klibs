647711731319a8c6e31b9b8a8821f4a7a9441d8f
J. Mulle <this.impetus@gmail.com>
2016-04-19 12:47:53 -0300

    Fixed bug in white-space handling of TrialFactory

--- klibs/KLTrialFactory.py
+++ klibs/KLTrialFactory.py
@@ -166,6 +166,8 @@ class TrialFactory(object):
 						continue
 				except IndexError:
 					pass
+				for i in range(0, len(row)):
+					row[i] = row[i].replace(" ", "")
 				self.config_file_rows.append(row)
 				self.__parse_parameters_row(row, header= row_count == 0)
 				row_count += 1

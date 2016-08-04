f4ec0a85f2ab2558ac85c4c8405c82fb001450d0
Austin Hurst <ray@rayD25HP0TSDHJR.local>
2016-08-04 15:05:13 -0300

    Fixed AnnulusBoundary responses

--- klibs/KLDraw.py
+++ klibs/KLDraw.py
@@ -311,15 +311,14 @@ class Rectangle(Drawbject):
 			self.draw()
 
 	def draw(self, as_numpy_surface=False):
-		x1 = self.stroke_width + 1
-		y1 = self.stroke_width + 1
-		x2 = self.surface_width - (self.stroke_width + 1)
-		y2 = self.surface_height - (self.stroke_width + 1)
+		xy1 = (self.stroke_width // 2) + 1
+		x2 = self.surface_width - ((self.stroke_width // 2) + 1)
+		y2 = self.surface_height - ((self.stroke_width // 2) + 1)
 		if self.stroke:
 			if self.fill:
-				self.surface.rectangle((x1, y1, x2, y2), self.stroke, self.fill)
+				self.surface.rectangle((xy1, xy1, x2, y2), self.stroke, self.fill)
 			else:
-				self.surface.rectangle((x1, y1, x2, y2), self.stroke)
+				self.surface.rectangle((xy1, xy1, x2, y2), self.stroke)
 		else:
 			self.surface.rectangle((1, 1, self.surface_width - 1, self.surface_height - 1), self.fill)
 

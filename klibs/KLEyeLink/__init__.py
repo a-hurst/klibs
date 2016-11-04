e0a9f885d79fd8486c70e167cf5629e44dfc986a
kleinLabAlpha <ray@Rays-iMac.local>
2016-11-04 14:31:51 -0300

    Bug where ELx flag would not work when PyLink was present fixed

--- klibs/KLEyeLink/__init__.py
+++ klibs/KLEyeLink/__init__.py
@@ -4,7 +4,7 @@ __author__ = 'j. mulle, this.impetus@gmail.com'
 from klibs import PYLINK_AVAILABLE
 from klibs import P
 
-if PYLINK_AVAILABLE:
+if PYLINK_AVAILABLE and P.eye_tracker_available:
 	from pylink import EyeLink, openGraphicsEx, flushGetkeyQueue, beginRealTimeMode, EyeLinkCustomDisplay, KeyInput, \
 		DC_TARG_BEEP, CAL_TARG_BEEP, CAL_ERR_BEEP, DC_ERR_BEEP, ENTER_KEY, ESC_KEY
 	from pylink.tracker import Sample, EndSaccadeEvent, EndFixationEvent, StartFixationEvent, StartSaccadeEvent

5b0335055c95a204d4e7078306914a83e24313e6
Austin Hurst <mynameisaustinhurst@gmail.com>
2017-10-03 16:55:37 -0300

    *vastly* simplifed camera image drawing logic

--- klibs/KLEyeLink/KLCustomEyeLinkDisplay.py
+++ klibs/KLEyeLink/KLCustomEyeLinkDisplay.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 __author__ = 'j. mulle, this.impetus@gmail.com'
 
-import array
-import sys
 from klibs import PYLINK_AVAILABLE
 
 if PYLINK_AVAILABLE:
@@ -12,14 +10,13 @@ if PYLINK_AVAILABLE:
 
 	from sdl2 import SDL_KEYDOWN, SDLK_ESCAPE, SDLK_RETURN, SDLK_c, SDLK_v
 	from numpy import asarray
-	from aggdraw import Draw, Pen
-	from PIL.Image import frombytes
+	from PIL import Image
 
 	from klibs.KLEnvironment import EnvAgent
 	from klibs import P
 	from klibs.KLUtilities import pump, mouse_pos
 	from klibs.KLUserInterface import ui_request
-	from klibs.KLGraphics import fill, flip, blit, aggdraw_to_array
+	from klibs.KLGraphics import fill, flip, blit
 	from klibs.KLGraphics.KLDraw import drift_correct_target
 	from klibs.KLAudio import AudioClip  # just a simple class for playing sdl2 sounds we made
 
@@ -28,14 +25,9 @@ if PYLINK_AVAILABLE:
 		def __init__(self):
 			EnvAgent.__init__(self)
 			self.size = (0,0)
-			self.last_flip = None
-			if sys.byteorder == 'little':
-				self.byteorder = 1
-			else:
-				self.byteorder = 0
-			self.imagebuffer = array.array('I')
-			self.pal = None
-			self.__img__ = None
+			self.raw_size = (0,0)
+			self.imagebuffer = []
+			self.palette = None
 
 			EyeLinkCustomDisplay.__init__(self)
 
@@ -48,12 +40,6 @@ if PYLINK_AVAILABLE:
 				self.__target_beep__done__ = None
 				self.__target_beep__error__ = None
 
-		def setup_cal_display(self):
-			self.clear_cal_display()
-
-		def exit_cal_display(self):
-			self.clear_cal_display()
-
 		def record_abort_hide(self):
 			pass
 
@@ -62,7 +48,10 @@ if PYLINK_AVAILABLE:
 			flip()
 			fill()
 
-		def erase_cal_target(self):
+		def setup_cal_display(self):
+			self.clear_cal_display()
+
+		def exit_cal_display(self):
 			self.clear_cal_display()
 
 		def draw_cal_target(self, x, y=None, pump_events=True):
@@ -71,8 +60,11 @@ if PYLINK_AVAILABLE:
 			if y is None:
 				y = x[1]
 				x = x[0]
-			blit(drift_correct_target(), 5, (int(x), int(y))
+			blit(drift_correct_target(), 5, (int(x), int(y)))
 			flip()
+
+		def erase_cal_target(self):
+			self.clear_cal_display()
 
 		def play_beep(self, clip):
 			try:
@@ -104,61 +96,45 @@ if PYLINK_AVAILABLE:
 		def get_mouse_state(self):
 			return mouse_pos()
 
-		def exit_image_display(self):
-			self.clear_cal_display()
-
 		def alert_printf(self, message):
-			message(message, color=(255, 0, 0, 0),
+			message(message, color=(255, 0, 0, 255),
 									location=(0.05 * P.screen_x, 0.05 * P.screen_y))
 
 		def setup_image_display(self, width, height):
 			self.size = (width, height)
 			self.clear_cal_display()
 
+		def exit_image_display(self):
+			self.clear_cal_display()
+
 		def image_title(self, text):
 			pass
 
+		def set_image_palette(self, r, g, b):
+			'''
+			Sets the palette to use for the camera image and clears the image buffer.
+			Converts r,g,b (lists containing the RGB palette) to a list of colours
+			([R,G,B,R,G,B,...]) that can be used by PIL.Image.
+			'''
+			self.imagebuffer = []
+			self.palette = list(sum(zip(r,g,b), ()))
+
 		def draw_image_line(self, width, line, totlines, buff):
-			i = 0
-			while i < width:
-				if buff[i] >= len(self.pal):
-					buff[i] = len(self.pal) - 1
-				self.imagebuffer.append(self.pal[buff[i] & 0x000000FF])
-				i += 1
-			if width == totlines:
+			if self.raw_size != (width, totlines):
+				self.raw_size = (width, totlines)
+			self.imagebuffer += buff
+			if int(line) == int(totlines):
 				fill()
-				img = frombytes('RGBX', (width, totlines), self.imagebuffer.tostring()).convert('RGBA')
-				blit(img, registration=5, location=P.screen_c)
+				img = Image.new("P", self.raw_size, 0)
+				img.putpalette(self.palette)
+				img.putdata(self.imagebuffer)
+				blit(asarray(img.convert('RGBA').resize(self.size)), 5, position=P.screen_c)
 				flip()
-				self.imagebuffer = array.array('I')
+				self.imagebuffer = []
 
 		def draw_lozenge(self, x, y, width, height, colorindex):
 			pass
 
 		def draw_line(self, x1, y1, x2, y2, colorindex):
 			# maybe use opengl quads instead for simplicity?
-			try:
-				print "Draw Line: {0}".format(x1, x2, y1, y2, colorindex)
-				line = Draw("RGBA", [x2 - x1, y2 - y1])
-				p = Pen((255, 255, 255), 2)
-				line.line((0, 0, x2, y2), p)
-				blit(aggdraw_to_array(line), registration=5, location=P.screen_c)
-			except:
-				pass
-
-		def set_image_palette(self, r, g, b):
-			self.imagebuffer = array.array('I')
-			sz = len(r)
-			i = 0
-			self.pal = []
-			while i < sz:
-				rf = int(b[i])
-				gf = int(g[i])
-				bf = int(r[i])
-				if self.byteorder:
-					self.pal.append((rf << 16) | (gf << 8) | (bf))
-				else:
-					self.pal.append((bf << 24) | (gf << 16) | (rf << 8)) #for mac
-				i += 1
-
-
+			pass

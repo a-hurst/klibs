f76e71b130ba602783ed17eb0a94ba1f4f2771ca
Austin Hurst <mynameisaustinhurst@gmail.com>
2017-12-17 00:59:19 -0400

    updating/cleanup of audio response code

--- klibs/KLAudio.py
+++ klibs/KLAudio.py
@@ -1,61 +1,50 @@
 # -*- coding: utf-8 -*-
 __author__ = 'j. mulle, this.impetus@gmail.com'
 
+import os
+import sys
 import warnings
 import math
+from array import array
 
 with warnings.catch_warnings():
 	warnings.simplefilter("ignore")
 	import sdl2.ext
 	from sdl2.sdlmixer import (Mix_LoadWAV, Mix_PlayChannel, Mix_Playing, Mix_HaltChannel, 
 		Mix_VolumeChunk)
-	warnings.simplefilter("default")
 
+from klibs.KLEnvironment import EnvAgent
 from klibs.KLConstants import (AUDIO_ON, AUDIO_OFF, AR_CHUNK_READ_SIZE, AR_CHUNK_SIZE, AR_RATE,
 	AR_THRESHOLD, AR_AUTO_THRESHOLD)
 from klibs import P
-from klibs.KLUtilities import pump, flush
+from klibs.KLUtilities import pump, flush, peak
+from klibs.KLTime import CountDown
 from klibs.KLUserInterface import ui_request
 from klibs.KLGraphics.KLDraw import Ellipse
 from klibs.KLGraphics import fill, blit, flip
-
+from klibs.KLCommunication import message
 from klibs import PYAUDIO_AVAILABLE
+
 if PYAUDIO_AVAILABLE:
 	import pyaudio
-	from array import array
-
-
-if P.audio_initialized is False:
-	sdl2.SDL_Init(sdl2.SDL_INIT_AUDIO)
-	sdl2.sdlmixer.Mix_OpenAudio(44100, sdl2.sdlmixer.MIX_DEFAULT_FORMAT, 2, 1024)
-	P.audio_initialized = True
 
 
 class AudioManager(object):
-	listeners = {}
 
 	def __init__(self):
-		"""
-
-
-		"""
 		super(AudioManager, self).__init__()
+		if not sdl2.SDL_WasInit(sdl2.SDL_INIT_AUDIO):
+			sdl2.SDL_Init(sdl2.SDL_INIT_AUDIO)
+			sdl2.sdlmixer.Mix_OpenAudio(44100, sdl2.sdlmixer.MIX_DEFAULT_FORMAT, 2, 1024)
+		if PYAUDIO_AVAILABLE:
+			self.input = pyaudio.PyAudio()
+		else:
+			self.input = None
 
-	# def create_listener(self, name=None, threshold=AR_AUTO_THRESHOLD):
-	# 	if not PYAUDIO_AVAILABLE:
-	# 		raise RuntimeError("PyAudio module not loaded; KLAudio.AudioResponseListener not available.")
-	# 	listener = AudioResponseListener(, threshold)
-	# 	if name:
-	# 		self.listeners[name] = listener
-	# 	return listener
-
-	def clip(self, file_path):
-		"""
-
-		:param file_path:
-		:return:
-		"""
-		return AudioClip(file_path)
+	def open(self, *args, **kwargs):
+		if not PYAUDIO_AVAILABLE:
+			raise RuntimeError("The PyAudio module is not installed; audio input is not available.")
+		return self.input.open(*args, **kwargs)
 
 
 # Note AudioClip is an adaption of code originally written by mike lawrence (github.com/mike-lawrence)
@@ -72,7 +61,6 @@ class AudioClip(object):
 			:param file_path:
 			"""
 			super(AudioClip, self).__init__()
-			self.sample = Mix_LoadWAV(sdl2.ext.compat.byteify(file_path, "utf-8"))
 			self.started = False
 			self.channel = self.default_channel
 
@@ -177,8 +165,7 @@ class AudioSample(object):
 		return self.trough > threshold if threshold else self.threshold
 
 
-class AudioStream(object):
-	p = None
+class AudioStream(EnvAgent):
 	stream = None
 
 	def __init__(self, threshold=1):
@@ -187,7 +174,6 @@ class AudioStream(object):
 		:param threshold:
 		"""
 		super(AudioStream, self).__init__()
-		self.p = pyaudio.PyAudio()
 		self.threshold = 1
 		# if threshold == AR_AUTO_THRESHOLD:
 		# 	self.threshold = 3 * self.get_ambient_level()  # this is probably inadequate and should employ a log scale
@@ -202,7 +188,6 @@ class AudioStream(object):
 		"""
 		if not self.stream:
 			self.init_stream()
-		# try:
 
 		chunk = self.stream.read(AR_CHUNK_SIZE, False)
 		sample = AudioSample(chunk, self.threshold)
@@ -212,88 +197,83 @@ class AudioStream(object):
 		# 	return AudioSample(self.stream.read(AR_CHUNK_SIZE), P.AR_AUTO_THRESHOLD)
 
 	def init_stream(self):
-		"""
 
-
-		"""
 		try:
 			self.kill_stream()
 		except (AttributeError, IOError) as e:
 			pass  # on first pass, no stream exists; on subsequent passes, extant stream should be stopped & overwritten
-
-		self.p = pyaudio.PyAudio()
-		self.stream = self.p.open(format=pyaudio.paInt16, channels=1, rate=AR_RATE, input=True, output=True, \
-															  frames_per_buffer=AR_CHUNK_SIZE)
+		
+		# Due to using a depricated macOS API, portaudio will print a warning everytime it's called.
+		# The following lines suppress this error.
+		devnull = os.open(os.devnull, os.O_WRONLY)
+		old_stderr = os.dup(2)
+		sys.stderr.flush()
+		os.dup2(devnull, 2)
+		os.close(devnull)
+		self.stream = self.exp.audio.open(
+			format=pyaudio.paInt16, channels=1, rate=AR_RATE,
+			input=True, output=True, frames_per_buffer=AR_CHUNK_SIZE
+		)
+		os.dup2(old_stderr, 2)
+		os.close(old_stderr)
 
 	def kill_stream(self):
-			self.stream.stop_stream()
-			self.stream.close()
-			self.p.terminate()
+		self.stream.stop_stream()
+		self.stream.close()
 
 	def get_ambient_level(self, period=1):
-
-		"""
-
-		:param period:
-		:return:
-		"""
-		sample_period = P.tk.countdown(period)
-		warn_message = "Please remain quite while the ambient noise level is sampled. Sampling will begin in 3 seconds."
-		sampling_message = "Sampling Complete In {0} Seconds"
+		
+		warn_msg = ("Please remain quite while the ambient noise level is sampled. "
+					"Sampling will begin in {0} seconds.")
 		peaks = []
 
+		wait_period = CountDown(3)
+		while wait_period.counting():
+			ui_request()
+			fill()
+			remaining = int(math.ceil(wait_period.remaining()))
+			message(warn_msg.format(remaining), location=P.screen_c, registration=5)
+			flip()
+		
 		fill()
-		#message(warn_message, location=P.screen_c, registration=5)
 		flip()
-		sample_period.start_clock()
-		#message(sampling_message.format(int(math.ceil(sample_period.remaining())), font_size="48pt"), location=P.screen_c, registration=5)
-
+		sample_period = CountDown(period)
 		while sample_period.counting():
 			ui_request()
 			peaks.append(self.sample().peak)
 
 		return sum(peaks) / len(peaks)
 
-	def get_peak_during(self, period=3, message=None):
-		"""
+	def get_peak_during(self, period=3, msg=None):
 
-		:param period:
-		:param message:
-		:return:
-		"""
-		# initial_diameter = int(P.screen_x * 0.05)
 		local_peak = 0
-		pump()
+		last_sample = 0
+		if msg:
+			msg = message(msg, blit_txt=False)
+		
 		flush()
-
-		sample_period = P.tk.countdown(period)
-		first_flip_rest = False
-		if message:
-			pass
-			#message = .message(message, location=P.screen_c, registration=5, blit=False)
-		if not self.stream:
-			self.init_stream()
 		self.init_stream()
+		self.sample() # eat first part of stream to avoid
+		sample_period = CountDown(period+0.05)
 		while sample_period.counting():
-			sample = self.sample().peak
 			ui_request()
-			fill()
+			sample = self.sample().peak
+			if sample_period.elapsed() < 0.05:
+				# Sometimes 1st or 2nd peaks are extremely high for no reason, so ignore first 50ms
+				continue
 			if sample > local_peak:
 				local_peak = sample
-			peak_circle = int((local_peak * P.screen_x * 0.9) / 65000)
-			sample_circle = int((sample * P.screen_x * 0.9) / 65000)
-			if peak_circle < 5:
-				peak_circle = 5
-			if sample_circle < 5:
-				sample_circle = 5
-			if message:
-				blit(message, location=[25,25], registration=7)
+			sample_avg = (sample + last_sample) / 2
+			peak_circle = peak(5, int((local_peak*P.screen_x*0.9) / 65000))
+			sample_circle = peak(5, int((sample_avg*P.screen_x*0.9) / 65000))
+			last_sample = sample
+			
+			fill()
 			blit(Ellipse(peak_circle, fill=[255, 145, 0]), location=P.screen_c, registration=5)
 			blit(Ellipse(sample_circle, fill=[84, 60, 182]), location=P.screen_c, registration=5)
+			if msg:
+				blit(msg, location=[25,25], registration=7)
 			flip()
-			if not first_flip_rest:
-				sample_period.start_clock()
-				first_flip_rest = True
 		self.kill_stream()
 
 		return local_peak

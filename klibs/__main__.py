62d2bd46f664114d36102ba2e12c537e7885536e
Austin Hurst <mynameisaustinhurst@gmail.com>
2021-02-04 14:34:37 -0400

    Only load external deps when launching runtime

--- klibs/__main__.py
+++ klibs/__main__.py
@@ -240,6 +240,12 @@ def run(screen_size, path, condition, devmode, no_tracker, seed):
 
 	cso("\n\n<green>*** Now Loading KLibs Environment ***</green>\n")
 
+	# Suppresses possible pysdl2-dll warning message on import
+	import warnings
+	with warnings.catch_warnings():	
+		warnings.simplefilter("ignore")
+		import sdl2
+
 	from klibs import env
 	from klibs.KLGraphics.core import display_init
 	from klibs.KLDatabase import DatabaseManager

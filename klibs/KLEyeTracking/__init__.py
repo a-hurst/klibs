e5cfe7924e6df2de6c79d1e59f2236cff432a05e
Austin Hurst <mynameisaustinhurst@gmail.com>
2021-02-05 15:31:09 -0400

    Future-proofed package availability checks

--- klibs/KLEyeTracking/__init__.py
+++ klibs/KLEyeTracking/__init__.py
@@ -1,20 +1,18 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Jonathan Mulle & Austin Hurst'
 
-import imp
 from klibs import P
-try:
-	imp.find_module('pylink')
-	PYLINK_AVAILABLE = True
-except ImportError:
-	print("\t* Warning: Pylink library not found; eye tracking will not be available.")
-	PYLINK_AVAILABLE = False
+from klibs.KLInternal import package_available
+
+PYLINK_AVAILABLE = package_available('pylink')
 
 if PYLINK_AVAILABLE and P.eye_tracker_available:
+	if P.development_mode:
+		print("* Pylink available, attempting to use EyeLink eye tracker...\n")
 	from .KLEyeLink import EyeLink as Tracker
-	if P.development_mode:
-		print("Using PyLink")
 else:
+	if P.eye_tracker_available:
+		print("* Warning: Pylink library not installed, falling back to TryLink\n")
+	elif P.development_mode:
+		print("* Using TryLink mouse simulation\n")
 	from .KLTryLink import TryLink as Tracker
-	if P.development_mode:
-		print("Using TryLink")

14f79dc2203b251ea206c46c946715cdffdae9a0
Austin Hurst <mynameisaustinhurst@gmail.com>
2022-04-23 20:23:24 -0300

    Make filepath orders consistent

--- klibs/KLParams.py
+++ klibs/KLParams.py
@@ -145,15 +145,15 @@ font_dirs = None  # Dynamically set at runtime
 
 # Project Filepaths (dynamically set at runtime)
 database_path = None
-database_local_path = None
 database_backup_path = None
-ind_vars_file_path = None
-ind_vars_file_local_path = None
-log_file_path = None
+database_local_path = None
 params_file_path = None
 params_local_file_path = None
+ind_vars_file_path = None
+ind_vars_file_local_path = None
 schema_file_path = None
 user_queries_file_path = None
+log_file_path = None
 logo_file_path = None
 
 
@@ -163,13 +163,13 @@ def initialize_paths(exp_name):
 	
 	global database_path
 	global database_backup_path
-	global ind_vars_file_path
-	global ind_vars_file_local_path
-	global log_file_path
 	global params_file_path
 	global params_local_file_path
+	global ind_vars_file_path
+	global ind_vars_file_local_path
 	global schema_file_path
 	global user_queries_file_path
+	global log_file_path
 
 	# Set experiment name globally in module
 	project_name = exp_name
@@ -184,15 +184,15 @@ def initialize_paths(exp_name):
 	log_filename = "{0}_log.txt".format(project_name)
 
 	# Initialize project file paths
-	log_file_path = join(asset_dir, log_filename)
-	schema_file_path = join(config_dir, schema_filename)
-	user_queries_file_path = join(config_dir, user_queries_filename)
 	database_path = join(asset_dir, database_filename)
 	database_backup_path = join(asset_dir, database_backup_filename)
-	ind_vars_file_path = join(config_dir, ind_vars_filename)
-	ind_vars_file_local_path = join(local_dir, ind_vars_filename)
 	params_file_path = join(config_dir, params_filename)
 	params_local_file_path = join(local_dir, params_filename)
+	ind_vars_file_path = join(config_dir, ind_vars_filename)
+	ind_vars_file_local_path = join(local_dir, ind_vars_filename)
+	schema_file_path = join(config_dir, schema_filename)
+	user_queries_file_path = join(config_dir, user_queries_filename)
+	log_file_path = join(asset_dir, log_filename)
 
 
 def initialize_runtime(exp_name, randseed):

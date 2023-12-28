c467e72864d82720715818326cae79e06ac18f4c
Austin Hurst <mynameisaustinhurst@gmail.com>
2023-12-28 18:17:48 -0400

    Fix blit & clear indentation

--- klibs/KLGraphics/core.py
+++ klibs/KLGraphics/core.py
@@ -164,113 +164,113 @@ def fill(color=None, context=None):
 
 
 def blit(source, registration=7, location=(0,0), flip_x=False):
-        """
-        Draws passed content to the display buffer. All content that is not in already
-        rendered to Numpy Array format will be rendered when it is passed to this
-        function, thus it is recommended to render beforehand whenever possible to
-        avoid performance issues resulting from the extra overhead.
+    """
+    Draws passed content to the display buffer. All content that is not in already
+    rendered to Numpy Array format will be rendered when it is passed to this
+    function, thus it is recommended to render beforehand whenever possible to
+    avoid performance issues resulting from the extra overhead.
 
-        Valid source content types include :obj:`NumpySurface` objects, :obj:`Drawbject` shapes,
-        and :obj:`numpy.ndarray` or :obj:`Pillow.Image` objects in RGBA format.
+    Valid source content types include :obj:`NumpySurface` objects, :obj:`Drawbject` shapes,
+    and :obj:`numpy.ndarray` or :obj:`Pillow.Image` objects in RGBA format.
 
-        Args:
-            source: Image data to draw to the display buffer.
-            registration (int): An integer from 1 to 9 indicating which location on the
-                surface will be aligned to the location value (see manual for more info).
-            location(tuple(int,int)): A tuple of x,y pixel coordinates indicating where to
-                draw the object to.
-            flip_x (bool): If True, flips the content along its x-axis before drawing to
-                the display buffer.
-        
-        Raises:
-            TypeError: If the 'source' object passed is not one of the accepted types.
+    Args:
+        source: Image data to draw to the display buffer.
+        registration (int): An integer from 1 to 9 indicating which location on the
+            surface will be aligned to the location value (see manual for more info).
+        location(tuple(int,int)): A tuple of x,y pixel coordinates indicating where to
+            draw the object to.
+        flip_x (bool): If True, flips the content along its x-axis before drawing to
+            the display buffer.
+    
+    Raises:
+        TypeError: If the 'source' object passed is not one of the accepted types.
 
-        """
-        # TODO: Add reference to location/registration explanation in the docstring once it's written
-        if isinstance(source, NumpySurface):
-            height = source.height
-            width = source.width
+    """
+    # TODO: Add reference to location/registration explanation in the docstring once it's written
+    if isinstance(source, NumpySurface):
+        height = source.height
+        width = source.width
+        content = source.render()
+
+    elif isinstance(source, Image.Image):
+        # is this a good idea? will be slower in most cases than using np.asarray() on Image
+        # and rendering that, since you don't need to re-render every time.
+        height = source.size[1]
+        width = source.size[0]
+        content = source.tobytes("raw", "RGBA", 0, 1)
+
+    elif issubclass(type(source), Drawbject):
+        height = source.surface_height
+        width = source.surface_width
+        if source.rendered is None:
             content = source.render()
-
-        elif isinstance(source, Image.Image):
-            # is this a good idea? will be slower in most cases than using np.asarray() on Image
-            # and rendering that, since you don't need to re-render every time.
-            height = source.size[1]
-            width = source.size[0]
-            content = source.tobytes("raw", "RGBA", 0, 1)
-
-        elif issubclass(type(source), Drawbject):
-            height = source.surface_height
-            width = source.surface_width
-            if source.rendered is None:
-                content = source.render()
-            else:
-                content = source.rendered
-
-        elif type(source) is np.ndarray:
-            height = source.shape[0]
-            width = source.shape[1]
-            content = source
-
         else:
-            raise TypeError("source must be an ndarray, NumpySurface, or be a KLibs Drawbject.")
-        
-        if any([not flip_x and P.blit_flip_x, flip_x]):
-            content = np.fliplr(content)
+            content = source.rendered
 
-        # Create and initialize OpenGL texture from source
-        # TODO: Add support for texture caching
-        gl.glEnable(gl.GL_TEXTURE_2D)
-        t_id = gl.glGenTextures(1)
-        gl.glBindTexture(gl.GL_TEXTURE_2D, t_id)
-        gl.glTexEnvi(gl.GL_TEXTURE_ENV, gl.GL_TEXTURE_ENV_MODE, gl.GL_REPLACE)
-        gl.glTexParameterf(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_S, gl.GL_CLAMP)
-        gl.glTexParameterf(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_T, gl.GL_CLAMP)
-        gl.glTexParameterf(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_LINEAR)
-        gl.glTexParameterf(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_LINEAR)
-        gl.glTexImage2D(
-            gl.GL_TEXTURE_2D, 0, gl.GL_RGBA, width, height, 0, gl.GL_RGBA,
-            gl.GL_UNSIGNED_BYTE, content
-        )
+    elif type(source) is np.ndarray:
+        height = source.shape[0]
+        width = source.shape[1]
+        content = source
 
-        # location[0] += P.screen_origin[0]
-        # location[1] += P.screen_origin[1]
-        # define boundaries coordinates of region being blit to
-        x_bounds = [location[0], location[0] + width]
-        y_bounds = [location[1], location[1] + height]
+    else:
+        raise TypeError("source must be an ndarray, NumpySurface, or be a KLibs Drawbject.")
+    
+    if any([not flip_x and P.blit_flip_x, flip_x]):
+        content = np.fliplr(content)
 
-        # shift boundary mappings to reflect registration
-        #
-        # 7--8--9  Default assumes registration = 7, meaning that the top-left corner of the
-        # 4--5--6  texture is aligned to the pixel coordinates given by 'location'. Alignment
-        # 1--2--3  of the texture with the coordinates can be changed by choosing a different
-        #          anchor point from 1 to 9, as illustrated in the diagram to the left.
+    # Create and initialize OpenGL texture from source
+    # TODO: Add support for texture caching
+    gl.glEnable(gl.GL_TEXTURE_2D)
+    t_id = gl.glGenTextures(1)
+    gl.glBindTexture(gl.GL_TEXTURE_2D, t_id)
+    gl.glTexEnvi(gl.GL_TEXTURE_ENV, gl.GL_TEXTURE_ENV_MODE, gl.GL_REPLACE)
+    gl.glTexParameterf(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_S, gl.GL_CLAMP)
+    gl.glTexParameterf(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_T, gl.GL_CLAMP)
+    gl.glTexParameterf(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_LINEAR)
+    gl.glTexParameterf(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_LINEAR)
+    gl.glTexImage2D(
+        gl.GL_TEXTURE_2D, 0, gl.GL_RGBA, width, height, 0, gl.GL_RGBA,
+        gl.GL_UNSIGNED_BYTE, content
+    )
 
-        try:
-            x_offset, y_offset = _build_registrations(height, width)[registration]
-        except IndexError:
-            raise ValueError("Registration must be an integer between 1 and 9 inclusive")
+    # location[0] += P.screen_origin[0]
+    # location[1] += P.screen_origin[1]
+    # define boundaries coordinates of region being blit to
+    x_bounds = [location[0], location[0] + width]
+    y_bounds = [location[1], location[1] + height]
 
-        x_bounds[0] += int(x_offset)
-        x_bounds[1] += int(x_offset)
-        y_bounds[0] += int(y_offset)
-        y_bounds[1] += int(y_offset)
+    # shift boundary mappings to reflect registration
+    #
+    # 7--8--9  Default assumes registration = 7, meaning that the top-left corner of the
+    # 4--5--6  texture is aligned to the pixel coordinates given by 'location'. Alignment
+    # 1--2--3  of the texture with the coordinates can be changed by choosing a different
+    #          anchor point from 1 to 9, as illustrated in the diagram to the left.
 
-        gl.glBegin(gl.GL_TRIANGLE_STRIP)
-        gl.glTexCoord2f(0, 0)
-        gl.glVertex2f(x_bounds[0], y_bounds[0])
-        gl.glTexCoord2f(0, 1)
-        gl.glVertex2f(x_bounds[0], y_bounds[1])
-        gl.glTexCoord2f(1, 0)
-        gl.glVertex2f(x_bounds[1], y_bounds[0])
-        gl.glTexCoord2f(1, 1)
-        gl.glVertex2f(x_bounds[1], y_bounds[1])
-        gl.glEnd()
+    try:
+        x_offset, y_offset = _build_registrations(height, width)[registration]
+    except IndexError:
+        raise ValueError("Registration must be an integer between 1 and 9 inclusive")
 
-        gl.glBindTexture(gl.GL_TEXTURE_2D, 0)
-        gl.glDeleteTextures([t_id])
-        del t_id
-        gl.glDisable(gl.GL_TEXTURE_2D)
+    x_bounds[0] += int(x_offset)
+    x_bounds[1] += int(x_offset)
+    y_bounds[0] += int(y_offset)
+    y_bounds[1] += int(y_offset)
+
+    gl.glBegin(gl.GL_TRIANGLE_STRIP)
+    gl.glTexCoord2f(0, 0)
+    gl.glVertex2f(x_bounds[0], y_bounds[0])
+    gl.glTexCoord2f(0, 1)
+    gl.glVertex2f(x_bounds[0], y_bounds[1])
+    gl.glTexCoord2f(1, 0)
+    gl.glVertex2f(x_bounds[1], y_bounds[0])
+    gl.glTexCoord2f(1, 1)
+    gl.glVertex2f(x_bounds[1], y_bounds[1])
+    gl.glEnd()
+
+    gl.glBindTexture(gl.GL_TEXTURE_2D, 0)
+    gl.glDeleteTextures([t_id])
+    del t_id
+    gl.glDisable(gl.GL_TEXTURE_2D)
 
 
 def flip():
@@ -311,18 +311,18 @@ def flip():
 
 
 def clear(color=None):
-        """
-        Clears both current display and display buffer with a given color. If no color
-        is specified, the value of P.default_fill_colour is used.
+    """
+    Clears both current display and display buffer with a given color. If no color
+    is specified, the value of P.default_fill_colour is used.
 
-        Args:
-            color(iter, optional): An iterable of integers representing the RGB or RGBA
-                color value to be used to fill the display and display buffer (e.g.
-                color=(0,0,0) to fill both with black).
-        """
+    Args:
+        color(iter, optional): An iterable of integers representing the RGB or RGBA
+            color value to be used to fill the display and display buffer (e.g.
+            color=(0,0,0) to fill both with black).
+    """
 
-        if color is None: color = P.default_fill_color
-        fill(color)
-        flip()
-        fill(color)
-        flip()
+    if color is None: color = P.default_fill_color
+    fill(color)
+    flip()
+    fill(color)
+    flip()

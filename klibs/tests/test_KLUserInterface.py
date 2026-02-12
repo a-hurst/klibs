6d5990bda1ce4bc15a3dc935b5684abece97a8f9
Austin Hurst <mynameisaustinhurst@gmail.com>
2026-02-12 18:31:28 -0400

    Try fixing occasional CI failures

--- klibs/tests/test_KLUserInterface.py
+++ klibs/tests/test_KLUserInterface.py
@@ -252,18 +252,18 @@ def test_smart_sleep(with_sdl):
 
     # Queue a quit event and see if smart_sleep catches it
     queue_event(keydown('q', mod='ctrl'))
-    start = time.time()
+    start = time.perf_counter()
     ui.smart_sleep(1, units=TK_MS) # 1 millisecond
-    duration = time.time() - start
+    duration = time.perf_counter() - start
     assert round(duration, 5) >= 0.001
     #assert duration < 0.01  # Sometimes fails on CI runners?
     assert env.exp.command == 'quit'
 
     # Test units of seconds
     env.exp.command = None
-    start = time.time()
+    start = time.perf_counter()
     ui.smart_sleep(0.001, units=TK_S) # 1 millisecond
-    duration = time.time() - start
+    duration = time.perf_counter() - start
     assert round(duration, 5) >= 0.001
     #assert duration < 0.01  # Sometimes fails on CI runners?
     assert env.exp.command == None

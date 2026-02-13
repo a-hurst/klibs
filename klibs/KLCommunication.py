6494b735b975a00061b975ea70e13cb544fe91d1
Austin Hurst <mynameisaustinhurst@gmail.com>
2026-02-13 19:40:53 -0400

    Assume 'condition' runtime info column always present

--- klibs/KLCommunication.py
+++ klibs/KLCommunication.py
@@ -128,8 +128,6 @@ def collect_demographics(anonymous=False):
 
     # Log info about current runtime environment to database
     runtime_info = runtime_info_init()
-    if P.condition and "condition" in db.get_columns("session_info"):
-        runtime_info["condition"] = P.condition
     db.insert(runtime_info, "session_info")
 
     # Save copy of experiment.py and config files as they were for participant
